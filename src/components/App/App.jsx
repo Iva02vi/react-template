33712fd81db854cde8a96f4bc2160c4a0924fb6d
Iva02vi <139965283+Iva02vi@users.noreply.github.com>
2024-02-28 21:00:05 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+import "./App.module.css";
+// import Feedback from "../Feedback/Feedback.jsx";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
